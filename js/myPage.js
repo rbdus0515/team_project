2e0d566597370858db6796c7e24c31a4d233da42
Rabsterd <ksm6798@gmail.com>
2023-10-16 17:09:05 +0900

    10/16

--- js/myPage.js
+++ js/myPage.js
@@ -1,20 +1,31 @@
+
+
+
+// 모달
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const share = document.getElementsByClassName('share-button');
+  for (let i = 0; i < share.length; i++) {
+    share[i].addEventListener('click', () => {
+      modal.classList.toggle('show');
+    
+      if (modal.classList.contains('show')) {
+      }
+    });
 
-share.addEventListener('click', () => {
-  modal.classList.toggle('show');
-
-  if (modal.classList.contains('show')) {
   }
-});
 
-modal.addEventListener('click', (event) => {
-  if (event.target === modal) {
-    modal.classList.toggle('show');
-
-    if (!modal.classList.contains('show')) {
-      body.style.overflow = 'auto';
+  modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+      modal.classList.toggle('show');
+  
+      if (!modal.classList.contains('show')) {
+        body.style.overflow = 'auto';
+      }
     }
-  }
-});
+  });
+
+
+
+
+
