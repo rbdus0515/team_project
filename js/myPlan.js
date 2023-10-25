8b32c7f0ebb3e7c5b14265c67ddffb8ee193fc7d
Rabsterd <ksm6798@gmail.com>
2023-10-25 18:15:45 +0900

    10/25

--- js/myPlan.js
+++ js/myPlan.js
@@ -12,11 +12,40 @@ var map = new kakao.maps.Map(container, options);
 
 /* 선택한 여행지 div 숨기기 */
 const hidden = document.querySelector('.storage');
-const plan = document.querySelector('.left-side');
+const plan = document.querySelector('.right-side');
+const arrow = document.getElementById('arrow');
 
 hidden.addEventListener('click', () => {
 
-    plan.style.display = "none";
 
+    if(plan.style.display == 'none')  {
+        plan.style.display = 'block';
+        arrow.src = "/images/left-arrow.png";
+      }else {
+        plan.style.display = 'none';
+        arrow.src = "/images/right-arrow.png";
+      }
 });
+
+/* 출발지 div 펼치기 */
+const expend = document.querySelectorAll(".expend");
+const destinations = document.querySelectorAll(".destinations");
+var newContent = "-";
+for (let i = 0; i < expend.length; i++) {
+    expend[i].addEventListener('click', () => {
+    
+        for(let i = 0; i < destinations.length; i++) {
+
+            if(destinations[i].style.display == 'none') {
+                destinations[i].style.display = 'block';
+                expend.innerHTML = newContent;
+            }else {
+                destinations[i].style.display = 'none';
+                
+            }
+        }
+
+    })
+}
+
 
